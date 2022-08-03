001a45d09dfb8ed836a5c30ffafbf8edb444212e
Soledad Orozco <soledadorozco8@gmail.com>
2022-08-03 18:01:45 -0300

    buscador novedades

--- routes/novedades.js
+++ routes/novedades.js
@@ -4,7 +4,14 @@ var novedadesModel = require('../models/novedadesModel');
 
 router.get('/',async function(req,res,next){
 
-    var novedades  = await novedadesModel.getNovedades();
+    // var novedades  = await novedadesModel.getNovedades();
+    var novedades;
+
+    if(req.query.q === undefined){
+        novedades = await novedadesModel.getNovedades();
+    }else{
+        novedades = await novedadesModel.buscarNovedades(req.query.q);
+    }
     res.render('novedades',{
         isNovedades: true,
         novedades //de esta forma ya tengo los registros para poder imprimir en el hbs
