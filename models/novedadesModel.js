8b8e5e74d7a908ef2a960a0c42c4edb4111cae84
Soledad Orozco <soledadorozco8@gmail.com>
2022-08-04 02:10:14 -0300

    modifiqué novedades login, ordenar de forma ascen

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -1,7 +1,7 @@
 var pool = require('./bd');
 
 async function getNovedades(){ 
-        var query = 'select * from novedades';
+        var query = 'select * from novedades order by id ASC';
         var rows = await pool.query(query);
         return rows;
     }
